3c46381391853b013dc516912054defed67664af
kristoffer <kristoffer@ksture.com>
2020-03-21 22:21:27 +0100

    Added away, input and output status

--- src/ClientStatus.java
+++ src/ClientStatus.java
@@ -11,10 +11,21 @@ public class ClientStatus  {
 
         for (Client cli: clientList){
             if(cli.getType() == 0){
-                clients = clients.concat(cli.getNickname() + " (" + api.getChannelInfo(cli.getChannelId()).getName() +
-                        ")\n");
-            }
+                // Init input and output status
+                String micStatus = "not muted";
+                String outputStatus = "not muted";
 
+                if(cli.isInputMuted())
+                    micStatus = "muted";
+                else if(cli.isAway())
+                    micStatus = "away";
+
+                if(cli.isOutputMuted())
+                    outputStatus = "muted";
+
+                clients = clients.concat(cli.getNickname() + " (" + api.getChannelInfo(cli.getChannelId()).getName() +
+                        ") (" + micStatus + " | " + outputStatus + ")\n");
+            }a
         }
     }
 
