af37095f9b49f5bba5e65e9cceb46e3032173e6e
Kristoffer Sture <Kristure1993@gmail.com>
2020-03-25 14:36:44 +0100

    Initial commit

---
+++ src/PushMessage.java
@@ -0,0 +1,41 @@
+import net.pushover.client.*;
+
+public class PushMessage {
+    private String pushoverApi;
+    private String pushoverUserId;
+
+    public PushMessage(String pushoverApi, String pushoverUserId) {
+        this.pushoverApi = pushoverApi;
+        this.pushoverUserId = pushoverUserId;
+    }
+
+    void push(String message){
+        PushoverClient client = new PushoverRestClient();
+        String pushoverAPI = "aditr8h74zjadxxpwuc5s8ni28qsg5";
+        String pushoverUserId = "ur9nskmnd9cmaeaxvge18v6d1idrc5";
+
+        try{
+            client.pushMessage(PushoverMessage.builderWithApiToken(pushoverAPI)
+                    .setUserId(pushoverUserId)
+                    .setMessage(message)
+                    .build());
+        }catch (PushoverException e){
+            // Do nothing
+        }
+    }
+    void push(String message, int priority){
+        PushoverClient client = new PushoverRestClient();
+        String pushoverAPI = "aditr8h74zjadxxpwuc5s8ni28qsg5";
+        String pushoverUserId = "ur9nskmnd9cmaeaxvge18v6d1idrc5";
+
+        try{
+            client.pushMessage(PushoverMessage.builderWithApiToken(pushoverAPI)
+                    .setUserId(pushoverUserId)
+                    .setMessage(message)
+                    .setPriority(MessagePriority.HIGH)
+                    .build());
+        }catch (PushoverException e){
+            // Do nothing
+        }
+    }
+}
