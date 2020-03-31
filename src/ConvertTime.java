91a930e1c407ec2a1c06e526f464de879486f49d
Kristure <kristure1993@gmail.com>
2020-03-31 22:21:55 +0200

    Added ExecutorService that checks if client has gone idle or is back from being idle. If so send a PushMessage.

--- src/ConvertTime.java
+++ src/ConvertTime.java
@@ -2,24 +2,38 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ConvertTime {
-    private double time;
+    private long time;
 
     public String convert(long time) {
         this.time = time;
 
-        this.time = round(this.time / 60000, 2);
+        this.time = time / 1000;
 
-        if(time < 60000)
+        if(time < 1000)
             return "now";
-        else if(time < 3600000)
-            return this.time + " m";
+        else if(time < 60000){
+            this.time = (long)round(this.time, 0);
+            return this.time + " s";
+        }
+        else if(time < 3600000) {
+            int minutes = (int)round(this.time / 60, 0);
+            int seconds = (int)this.time % 60;
+            String minutesConverted = new ConcatTime().convert(minutes);
+            String secondsConverted = new ConcatTime().convert(seconds);
+            return minutesConverted + ":" + secondsConverted;
+        }
         else {
-            this.time = round(this.time / 60, 2);
-            return this.time + " h";
+            int hours = (int)round(this.time/3600, 0);
+            int minutes = (int)round((this.time/60)-(hours*60),0);
+            int seconds = (int)round((this.time-(hours*3600+minutes*60)),0);
+            String hoursConverted = new ConcatTime().convert(hours);
+            String minutesConverted = new ConcatTime().convert(minutes);
+            String secondsConverted = new ConcatTime().convert(seconds);
+            return hoursConverted + ":" + minutesConverted + ":" + secondsConverted;
         }
     }
 
-    private double round(double value, int decimals) {
+    private double round(long value, int decimals) {
         if (decimals < 0) throw new IllegalArgumentException();
 
         BigDecimal bd = BigDecimal.valueOf(value);
