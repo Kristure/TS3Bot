48db1c895f59415bf1c7c04ec45cac359fcdddda
kristoffer <kristoffer@ksture.com>
2020-04-03 14:45:35 +0200

    Moved production files into main package

--- src/main/Config.java
+++ src/main/Config.java
@@ -1,3 +1,5 @@
+package main;
+
 public class Config {
     public Boolean testing;
     public String queryPassword;
--- src/main/PushMessage.java
+++ src/main/PushMessage.java
@@ -1,3 +1,5 @@
+package main;
+
 import net.pushover.client.*;
 
 public class PushMessage {
