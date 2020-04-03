8d23d5dddb892a90656bb59a1fe1340bcdddd3da
kristoffer <kristoffer@ksture.com>
2020-04-03 15:13:40 +0200

    Added queryUsername to config.json and new class CountNormalUsers

--- src/main/Config.java
+++ src/main/Config.java
@@ -8,9 +8,11 @@ public class Config {
     public String pushoverApi;
     public String pushoverUserId;
     public boolean floodRateUnlimited;
+    public String queryUsername;
 
-    public Config(Boolean testing, Boolean floodRateUnlimited, String queryPassword, String server, Integer queryPort, String pushover_api, String pushover_user_id) {
+    public Config(Boolean testing, String queryUsername, Boolean floodRateUnlimited, String queryPassword, String server, Integer queryPort, String pushover_api, String pushover_user_id) {
         this.testing = testing;
+        this.queryUsername = queryUsername;
         this.floodRateUnlimited = floodRateUnlimited;
         this.queryPassword = queryPassword;
         this.server = server;
