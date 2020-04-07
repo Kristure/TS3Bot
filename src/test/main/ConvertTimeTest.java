351da5ea425b674e9ddc2d6e16d3b5b411dbc179
kristoffer <kristoffer@ksture.com>
2020-04-07 15:59:18 +0200

    Test added

---
+++ src/test/main/ConvertTimeTest.java
@@ -0,0 +1,28 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertTimeTest {
+
+    @Test
+    void convertOneHour() {
+        assertEquals("01:00:00", ConvertTime.convert(3600000));
+    }
+
+    @Test
+    void convertOneMinute() {
+        assertEquals("00:01:00", ConvertTime.convert(60000));
+    }
+
+    @Test
+    void convertFiveSeconds() {
+        assertEquals("00:00:05", ConvertTime.convert(5000));
+    }
+
+    @Test
+    void convertMixed() {
+        assertEquals("15:07:56", ConvertTime.convert(54476000));
+    }
+}
