d33eec32e79723de28d161a77e227eb8960f13ce
Kristure <Kristure1993@gmail.com>
2020-04-17 14:21:40 +0200

    FiveSecScheduler now replaced with OneSecScheduler

--- src/main/java/OneSecScheduler.java
+++ src/main/java/OneSecScheduler.java
@@ -1,10 +1,10 @@
 import com.github.theholywaffle.teamspeak3.TS3Api;
 
-public class FiveSecScheduler implements Runnable {
+public class OneSecScheduler implements Runnable {
     private final Config config;
     private final TS3Api api;
 
-    public FiveSecScheduler(Config config, TS3Api api) {
+    public OneSecScheduler(Config config, TS3Api api) {
         this.config = config;
         this.api = api;
     }
